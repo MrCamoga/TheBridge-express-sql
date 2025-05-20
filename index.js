2be8e3b7d05ee73eb826f7186bb15e9dd9dd11bc
MrCamoga <mrcamoga@gmail.com>
2025-05-20 02:36:40 +0200

    Added endpoint to create all tables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const express = require("express");
 
 const PORT = 25565;
@@ -6,40 +6,36 @@ const PORT = 25565;
 const app = express();
 app.use(express.json());
 
-const db = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "thebridge",
-        password: "",
-        database: "tb_express"
+let db;
+
+(async () => {
+	db = await mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "thebridge",
+		password: "",
+		database: "tb_express"
+	});
+})();
+
+app.get("/createTables", async (req,res) => {
+	const queries = [
+		"CREATE TABLE IF NOT EXISTS categories (id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL)",
+		"CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, description VARCHAR(150), category_id TINYINT UNSIGNED, CONSTRAINT category_ibfk_1 FOREIGN KEY (category_id) REFERENCES categories(id), KEY name (name))",
+		"CREATE TABLE IF NOT EXISTS users (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(200) NOT NULL, salt CHAR(32) NOT NULL, password CHAR(128) NOT NULL, creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)",
+		"CREATE TABLE IF NOT EXISTS orders (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, userid INT UNSIGNED NOT NULL, date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, CONSTRAINT userid_ibfk_1 FOREIGN KEY (userid) REFERENCES users(id))",
+		"CREATE TABLE IF NOT EXISTS orderproducts (orderid BIGINT UNSIGNED NOT NULL, productid INT UNSIGNED NOT NULL, quantity SMALLINT UNSIGNED NOT NULL, individual_price INT UNSIGNED NOT NULL, PRIMARY KEY (orderid,productid))"
+	];
+	try {
+		for(query of queries) {
+			const response = await db.execute(query);
+		}
+		res.send({message: "Tables created successfully"});
+	} catch(err) {
+		console.error(err);
+		res.status(500).send({message: err.code});
+	}
 });
-
-db.connect(err => {
-        if(err) {
-                console.log("An error ocurred connecting to the database");
-                throw err;
-        } else {
-                console.log("Connection to database stablished");
-        }
-})
-
-
-app.get("/createProductTable", (req,res) => {
-        const sql = `CREATE TABLE products (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, description VARCHAR(150), category_id TINYINT UNSIGNED, CONSTRAINT category_ibfk_1 FOREIGN KEY (category_id) REFERENCES categories(id), KEY name (name))`; // TODO test
-        db.query(sql, (err,result) => {
-                if(err) throw err;
-                res.send(result);
-        });
-});
-
-app.get("/createCategoryTable", (req,res) => {
-        const sql = `CREATE TABLE categories (id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL)`;
-        db.query(sql, (err,result) => {
-                if(err) throw err;
-                res.send(result);
-        });
-});
-
 
 app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
