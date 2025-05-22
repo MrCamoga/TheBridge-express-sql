18f40221ae0e2ffe45b68d96940aa348fa2b961c
MrCamoga <mrcamoga@gmail.com>
2025-05-22 22:50:55 +0200

    Added missing 404 in update user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -41,7 +41,8 @@ module.exports = {
 		const sql = `UPDATE users SET first_name = COALESCE(?,first_name), last_name = COALESCE(?,last_name), email = COALESCE(?,email) WHERE id = ?`;
 		try {
 			const [result,_] = await db().execute(sql, [first_name, last_name, email, id]); // TODO add password
-			res.send({message:"OK"});
+			if(result.affectedRows > 0) res.send({message:"OK"});
+			else res.status(404).send({message:"Not Found"});
 		} catch(err) {
 			if(err.code == "ER_DUP_ENTRY")
 				res.status(409).send({message: "Email already registered"});
